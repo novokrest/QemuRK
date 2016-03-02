33b03c6720ea492f3f03396595e9738b069bc114
novokrest <novokrest013@gmail.com>
2016-03-02 10:06:41 +0300

    update gdb script

--- bin/gdb-kbd-script.sh
+++ bin/gdb-kbd-script.sh
@@ -1,13 +1,17 @@
 #set breakpoints to possible keyboard-relative functions
 
-break hid_keyboard_poll
-break hid_keyboard_event
-break usb_hid_changed
-break ps2_put_keycode
+#break hid_keyboard_poll
+#break hid_keyboard_event
+#break usb_hid_changed
+#break ps2_put_keycode
+break usb_host_handle_fd
+break usb_host_handle_data
+break usb_host_req_complete_data
 
 #run qemu with usb-passthrough
-run -hda test.qcow2 -usb -device usb-host,hostbus=4,hostaddr=20
+run -enable-kvm -hda test.qcow2 -usb -device usb-host,hostbus=?,hostaddr=?
 
 #run qemu with usb-keyboard emulating
 run -hda test.qcow2 -usbdevice keyboard
 
+#o
